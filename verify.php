cb320ccac8da0f256f69f71800f81fd7f7baf663
wichian toso <dr.chian@hotmail.com>
2018-07-18 06:51:24 +0700

    re

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'eoy3Hd5JM5fe2dA7cKJanFfS6fhxTbDzykKpB5w2zBIM5qYzdQ1y9cWIPNCOyq6lQAbMtLr3sfQn3mRV2ETk1mgyWmWCEKIr+A6pEXQBstHgCf+0fd4jwmM1bBPwY+uSUvyBjPcCr9fSXtAKyhfsJAdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/oauth/verify';
+$url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
