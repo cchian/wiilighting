3eda8d5788b8f85f57087c35ab33ce095fc01b27
wichian toso <dr.chian@hotmail.com>
2018-07-18 01:32:49 +0700

    w

--- ESP_netpie_linebot/ESP_netpie_linebot.ino
+++ ESP_netpie_linebot/ESP_netpie_linebot.ino
@@ -1,6 +1,6 @@
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <MicroGear.h>
-#include <ESP8266HTTPClient.h>
+#include <HTTPClient.h>
 #include <ArduinoJson.h>
 const char* ssid     = "iGateWay"; //change this to your SSID
 const char* password = "34567890"; //change this to your PASSWORD
@@ -23,12 +23,12 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) { //
     msg[msglen] = '\0';
 Serial.println((char *)msg);
     if(*(char *)msg == '1'){
-        digitalWrite(LED_BUILTIN, LOW);   // LED on
+//        digitalWrite(LED_BUILTIN, LOW);   // LED on
         //microgear.chat(TargetWeb,"1");
         //send_data("ESP_LED_ON");
         send_json("ESP LED ON");
     }else{
-        digitalWrite(LED_BUILTIN, HIGH);  // LED off
+ //       digitalWrite(LED_BUILTIN, HIGH);  // LED off
       //microgear.chat(TargetWeb,"0");
       //send_data("ESP_LED_OFF");
       send_json("ESP LED OFF");
@@ -48,7 +48,7 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Starting...");
 
-    pinMode(LED_BUILTIN, OUTPUT);
+ //   pinMode(LED_BUILTIN, OUTPUT);
   
     if (WiFi.begin(ssid, password)) {
         while (WiFi.status() != WL_CONNECTED) {
@@ -63,7 +63,7 @@ Serial.println("WiFi connected");
 
     microgear.init(KEY,SECRET,ALIAS);
     microgear.connect(APPID);
-     digitalWrite(LED_BUILTIN, HIGH);   // LED on
+//     digitalWrite(LED_BUILTIN, HIGH);   // LED on
 }
 
 void send_json(String data){
